ac5e06993f62ebc31d72ff56fd101786affc14ef
Carter Clark <crtrclark@gmail.com>
2021-04-09 17:30:15 -0500

    added to state

--- src/controller/states/ArmedAway.java
+++ src/controller/states/ArmedAway.java
@@ -1,5 +1,47 @@
 package controller.states;
 
-public class ArmedAway {
+import controller.events.PressMotion;
+import controller.events.UncheckZone;
 
+public class ArmedAway extends SecurityState {
+	private static ArmedAway instance;
+
+	public static ArmedAway instance() {
+		if (instance == null) {
+			instance = new ArmedAway();
+		}
+		return instance;
+	}
+
+	/**
+	 * Processes the motion button being pressed
+	 */
+	@Override
+	public void handleEvent(PressMotion event) {
+
+	}
+
+	/**
+	 * Processes one zone being unchecked
+	 */
+	@Override
+	public void handleEvent(UncheckZone event) {
+
+	}
+
+	/**
+	 * initialize the state
+	 * 
+	 */
+	@Override
+	public void enter() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void leave() {
+		// TODO Auto-generated method stub
+
+	}
 }
