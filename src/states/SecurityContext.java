9f5be9e7c0bed87eac6b509901419a9bc3893f95
Carter Clark <crtrclark@gmail.com>
2021-04-26 16:16:47 -0500

    made progress

--- src/states/SecurityContext.java
+++ src/states/SecurityContext.java
@@ -16,25 +16,15 @@ import events.ZoneToggled;
 import zones.Zone;
 
 /**
+ * The context is an observer for the security system and stores the context
+ * info for states
  * 
- * @author Brahma Dathan and Sarnath Ramnath
+ * @author Ben Hines, Carter Clark, Chris Lara-Batencourt, Pavel Danek, Ricky
+ *         Nguyen
+ * 
+ *         Code adapted from Brahma Dathan and Sarnath Ramnath
  * @Copyright (c) 2010
- * 
- *            Redistribution and use with or without modification, are permitted
- *            provided that the following conditions are met:
- *
- *            - the use is for academic purpose only - Redistributions of source
- *            code must retain the above copyright notice, this list of
- *            conditions and the following disclaimer. - Neither the name of
- *            Brahma Dathan or Sarnath Ramnath may be used to endorse or promote
- *            products derived from this software without specific prior written
- *            permission.
- *
- *            The authors do not make any claims regarding the correctness of
- *            the code in this module and are not responsible for any loss or
- *            damage resulting from its use.
  */
-
 public class SecurityContext {
 	private static SecurityContext instance;
 	private Zone[] zones = new Zone[5]; // works for up to 5 zones
